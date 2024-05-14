f7dbf6f1498241bc0e233485c8b74a1ce3d2a625
“Magali <maguinavarro07@gmail.com>
2024-05-14 16:52:28 -0300

    Punto 1 terminado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+ a=10;
+ b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b); 
+
+
+Console.WriteLine("Ingrese un Numero:");
+string entrada = Console.ReadLine(); 
+bool anda= int.TryParse(entrada, out int num);
+
+
+if (anda)
+
+{
+int invertido= 0;
+ while (num > 0)
+{
+    int dig= num % 10;
+    invertido= (invertido * 10) + dig;
+    num = num / 10;
+}
+Console.WriteLine("El numero invertido es:" +invertido);
+}
+
+
+
+
+
+
+
+
+
